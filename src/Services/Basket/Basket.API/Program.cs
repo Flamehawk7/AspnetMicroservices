fc78a9dba334c1a6871cf6559baefb877d7f3de5
Nikolaos Bitounis <nikosmp1995@hotmail.com>
2023-08-03 19:38:33 +0300

    Ordering.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,25 +1,34 @@
+using Basket.API.Grpc_Services;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using StackExchange.Redis;
 using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
-var config = new ConfigurationBuilder();
-config.SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional:false, reloadOnChange: true);
 
-IConfiguration configuration = config.Build();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 builder.Services.AddStackExchangeRedisCache(options => options.Configuration = builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddScoped<DiscountGrpcService>();
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+    (o => o.Address = new Uri(builder.Configuration.GetValue<string>("GrpcSettings:DiscountUrl")));
+
 var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogCritical(builder.Configuration.GetValue<string>("GrpcSettings:DiscountUrl"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        public static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "Hawk", FirstName = "Nick", LastName = "Bitounis", EmailAddress = "nikosmp1995@hotmail.com", AddressLine = "Moon", Country = "Milkyway", TotalPrice = 350 }
+            };
+        }
+    }
+}
