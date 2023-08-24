f7e6864a9cf817cc23e5c7960c545bfd7b90ea49
Nikolaos Bitounis <nikosmp1995@hotmail.com>
2023-08-24 22:43:19 +0300

    Shopping.Aggrigator Added

--- src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -21,7 +21,7 @@ namespace Ordering.API.Controllers
 
         [HttpGet("{userName}", Name = "GetOrder")]
         [ProducesResponseType(typeof(IEnumerable<OrdersVm>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IEnumerable<OrdersVm>>> GetOrderByUserName(string userName)
+        public async Task<ActionResult<IEnumerable<OrdersVm>>> GetOrdersByUserName(string userName)
         {
             var query = new GetOrdersListQuery(userName);
             var orders = await _mediator.Send(query);
