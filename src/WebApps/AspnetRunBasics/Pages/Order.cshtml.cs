39af783b9ce2f2db9dfc3f43f4877316b4780c46
Nikolaos Bitounis <nikosmp1995@hotmail.com>
2023-08-25 15:47:12 +0300

    AspnetRunBasics Added

---
+++ src/WebApps/AspnetRunBasics/Pages/Order.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRunBasics
+{
+    public class OrderModel : PageModel
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderModel(IOrderService _orderService)
+        {
+            _orderService = _orderService ?? throw new ArgumentNullException(nameof(_orderService));
+        }
+
+        public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var userName = "Hawk";
+            Orders = await _orderService.GetOrdersByUserName(userName);
+
+            return Page();
+        }       
+    }
+}
